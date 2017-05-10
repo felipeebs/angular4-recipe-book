102644cf20edabc39452e79d9ea5cd2ed1a8f211
Felipe Schmitz <felipe.schmitz@sodet.biz>
2017-05-10 08:40:23 -0300

    Personal improvements to the makeshift navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Input() activeComponent: string;
   @Output() activeComponentChanged = new EventEmitter<string>();
 
   onChangeActiveComponent(component: string) {
