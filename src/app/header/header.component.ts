84c0489b421be9145604cc12682846975e35b0f6
Felipe Schmitz <felipe.schmitz@sodet.biz>
2017-06-02 16:05:54 -0300

    GETting Back the Recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,4 +17,8 @@ export class HeaderComponent {
       }
     );
   }
+
+  onLoadData() {
+    this.dataStorageService.loadRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { RecipesService } from '../recipes/recipes.service';
+
 @Injectable()
 export class DataStorageService {
   constructor(private  http: Http, private recipesService: RecipesService) {
@@ -8,5 +9,15 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put('https://ng-course-recipes.firebaseio.com/recipes.json', this.recipesService.getRecipes());
+  }
+
+  loadRecipes() {
+    return this.http.get('https://ng-course-recipes.firebaseio.com/recipes.json')
+      .subscribe(
+        (response: Response) => {
+          const recipes = response.json();
+          this.recipesService.setRecipes(recipes);
+        }
+      );
   }
 }
