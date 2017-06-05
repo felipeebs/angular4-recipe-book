1e47c27d659a1683d89c28abdbb0de2f3edd4792
Felipe Schmitz <felipe.schmitz@sodet.biz>
2017-06-05 15:51:38 -0300

    Possible Improvements (some)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,18 @@ import * as firebase from 'firebase';
 })
 export class AppComponent implements OnInit {
 
+  constructor(private authService: AuthService) {}
+
   ngOnInit() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBUxW2LWWmQD4y7vjN4Ju_X8YpImjT_0Ns',
       authDomain: 'ng-course-recipes.firebaseapp.com'
+    });
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('user', user);
+      if (user) {
+        this.authService.getToken();
+      }
     });
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -42,6 +42,7 @@ export class AuthService {
   }
 
   logOut() {
+    this.router.navigate(['/']);
     firebase.auth().signOut();
     this.token = null;
   }
