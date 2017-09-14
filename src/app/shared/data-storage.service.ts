4eae7d3eda2e430bfabd84323329020b6f9b940f
Felipe Schmitz <felipe.schmitz@sodet.biz>
2017-09-14 16:08:50 -0300

    Http Parameters

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import 'rxjs/Rx';
 
 import { RecipesService } from '../recipes/recipes.service';
@@ -16,7 +16,8 @@ export class DataStorageService {
   storeRecipes() {
     const token = this.authService.getToken();
     // const headers = new HttpHeaders().set('Authorization', 'Bearer test:1234');
-    return this.httpClient.put('https://ng-course-recipes.firebaseio.com/recipes.json?auth=' + token, this.recipesService.getRecipes(), {
+    return this.httpClient.put('https://ng-course-recipes.firebaseio.com/recipes.json', this.recipesService.getRecipes(), {
+      params: new HttpParams().set('auth', token),
       observe: 'body'
       // ,headers: headers
     });
@@ -25,7 +26,8 @@ export class DataStorageService {
   loadRecipes() {
     const token = this.authService.getToken();
     // return this.httpClient.get<Recipe[]>('https://ng-course-recipes.firebaseio.com/recipes.json?auth=' + token)
-    return this.httpClient.get<Recipe[]>('https://ng-course-recipes.firebaseio.com/recipes.json?auth=' + token, {
+    return this.httpClient.get<Recipe[]>('https://ng-course-recipes.firebaseio.com/recipes.json', {
+      params: new HttpParams().set('auth', token),
       observe: 'body',
       responseType: 'json'
     })
