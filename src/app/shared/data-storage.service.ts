c5f9ba70ce97385e774392454b318780e073405c
Felipe Schmitz <felipe.schmitz@sodet.biz>
2017-09-14 16:03:47 -0300

    Settings Headers

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/Rx';
 
 import { RecipesService } from '../recipes/recipes.service';
@@ -15,8 +15,10 @@ export class DataStorageService {
 
   storeRecipes() {
     const token = this.authService.getToken();
+    // const headers = new HttpHeaders().set('Authorization', 'Bearer test:1234');
     return this.httpClient.put('https://ng-course-recipes.firebaseio.com/recipes.json?auth=' + token, this.recipesService.getRecipes(), {
       observe: 'body'
+      // ,headers: headers
     });
   }
 
