3c407a9f28119fac2b96238b78c758fdc46d1405
Felipe Schmitz <felipe.schmitz@sodet.biz>
2017-09-22 13:33:56 -0300

    Selecting Data from State

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
@@ -10,19 +12,20 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
+  shoppingListState: Observable<{ingredients: Ingredient[]}>;
   private subscription: Subscription;
 
-  constructor(private shoppingListService: ShoppingListService) {
+  constructor(private shoppingListService: ShoppingListService,
+              private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) {
   }
 
   ngOnInit() {
-    this.ingredients = this.shoppingListService.getIngredients();
-    this.subscription = this.shoppingListService.ingredientsChanged
-        .subscribe(
-          (newList: Ingredient[]) =>
-            this.ingredients = newList
-        );
+    this.shoppingListState = this.store.select('shoppingList')
+    // this.subscription = this.shoppingListService.ingredientsChanged
+    //     .subscribe(
+    //       (newList: Ingredient[]) =>
+    //         this.ingredients = newList
+    //     );
   }
 
   ngOnDestroy() {
