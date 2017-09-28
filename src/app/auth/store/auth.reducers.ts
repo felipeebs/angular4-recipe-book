310dd0b3973913960a4b5946b1ecb3d8a9c1f1f4
Felipe Schmitz <felipe.schmitz@sodet.biz>
2017-09-28 10:21:35 -0300

    Adding Reducer Logic & Actions

--- src/app/auth/store/auth.reducers.ts
+++ src/app/auth/store/auth.reducers.ts
@@ -1,3 +1,4 @@
+import * as AuthActions from './auth.actions'
 
 export interface State {
   token: string,
@@ -9,6 +10,26 @@ const initialState: State = {
   isAuthenticated: false
 };
 
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case AuthActions.SIGNUP:
+    case AuthActions.SIGNIN:
+      return {
+        ...state,
+        isAuthenticated: true
+      };
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false
+      };
+    case AuthActions.SET_TOKEN:
+      return {
+        ...state,
+        token: action.payload
+      };
+    default:
+      return state;
+  }
 }
