f6aca9d9622ecc97351977318eb0d5074c1e8375
Felipe Schmitz <felipe.schmitz@sodet.biz>
2017-09-28 12:10:30 -0300

    Only React to Actions Once via take(1)

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -12,11 +12,13 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private store: Store<fromApp.AppState>) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return this.store.select('auth').switchMap((authState: fromAuth.State) => {
-      const copyOfReq = req.clone({
-        params: req.params.append('auth', authState.token)
-      })
-      return next.handle(copyOfReq);
-    });
+    return this.store.select('auth')
+               .take(1)
+               .switchMap((authState: fromAuth.State) => {
+                 const copyOfReq = req.clone({
+                   params: req.params.append('auth', authState.token)
+                 });
+                 return next.handle(copyOfReq);
+               });
   }
 }
